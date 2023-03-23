ed8507bd828d03a7c94f4d65771124292113b5c0
Willy <willy11221997@gmail.com>
2023-03-23 11:26:28 +0800

    first version

---
+++ JavaScript/test.js
@@ -0,0 +1,81 @@
+
+
+//alert('Hello World1');
+
+
+/*function alertx(x){
+    alert(x);
+}
+
+alertx(1);
+alertx(2);
+alertx(3);
+*/
+
+/*function calAge(year,birth){
+    var age = year -birth;
+    var temp = 'I was born at year' + birth + '. I am '
+    + age + ' years old. ';
+    return temp;
+}
+
+var x = calAge(2023, 1997);
+alert(x);
+*/
+
+/*
+function test(){
+    var people = ['jack','tim','peter'];
+    //people.push('jan');
+    for (i=0;i<3;i++){
+        alert(people[i]);
+    }
+
+}
+
+test()
+*/
+
+/*
+var person = {
+    name: 'jack',
+    age: 18,
+    job: 'IT',
+    schools: ['School A','School B','School C'],
+    personalStatement: function(){
+        var temp = 'My name is ' + this.name + ' and my job is ' + this.job;
+    return temp;
+    }
+}
+for (i=0;i<person.schools.length;i++){
+    alert(person.schools[i]);
+}
+
+var x = person.personalStatement();
+alert(x)
+*/
+
+/*
+var str =  'something test';
+var temp = str.substring(1,4);
+alert(temp);
+*/
+
+/*
+var str = 'This is some text';
+var temp = str.indexOf('text');
+alert(temp);
+*/
+
+/*
+console.log('123');
+*/
+
+/*
+setTimeout(function(){
+    window.location = 'www.google.com';
+},3000);
+*/
+
+
+
